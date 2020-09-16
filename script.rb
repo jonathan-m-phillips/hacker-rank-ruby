d3cd3ac96d40b4b742c13399d1211794c556e4d0
Jonathan Phillips <jonathan.m.phillips2@gmail.com>
2020-09-16 13:38:49 -0500

    learning how to use case in ruby sept 16

--- script.rb
+++ script.rb
@@ -58,6 +58,35 @@
 # Call the method coder.practice until coder.oh_one? becomes true.
 #   Use the until control structure.
 #   until is the logical equivalent of while not.
-until coder.oh_one?
-  coder.practice
+# until coder.oh_one?
+#   coder.practice
+# end
+
+# ----------------------------------------------
+
+# You have been given a function where an object which may or may not be of the
+#   above mentioned type is sent as an argument. You have to use the case control
+#   structure in Ruby to identify the class to which the object belongs and print
+#   the following output:
+
+# if Hacker, output "It's a Hacker!"
+# if Submission, output "It's a Submission!"
+# if TestCase, output "It's a TestCase!"
+# if Contest, output "It's a Contest!"
+# for any other object, output "It's an unknown model"
+
+def identify_class(obj)
+  # write your case control structure here
+  case obj
+  when Hacker
+    puts "It's a Hacker!"
+  when Submission
+    puts "It's a Submission!"
+  when TestCase
+    puts "It's a TestCase!"
+  when Contest
+    puts "It's a Contest!"
+  else
+    puts "It's an unknown model"
+  end
 end
