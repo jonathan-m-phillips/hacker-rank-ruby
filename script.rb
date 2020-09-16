ecc9ce6099b0fc58e5b349cfdc3d8aec350bbbf9
Jonathan Phillips <jonathan.m.phillips2@gmail.com>
2020-09-16 13:14:56 -0500

    learning range in ruby sept 16

--- script.rb
+++ script.rb
@@ -12,3 +12,7 @@
 # (0...gets.to_i).each do |i|
 #   puts odd_or_even(gets.to_i)
 # end
+
+# Three variables a, b, and c are already defined. Your task is to write code that checks whether
+#   a is within the range of b and c by calling the method range?
+a.range?(b,c)
