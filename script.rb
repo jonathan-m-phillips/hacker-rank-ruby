f623ec1505f1520b4b333ae2e56485a810a736b3
Jonathan Phillips <jonathan.m.phillips2@gmail.com>
2020-09-16 13:00:33 -0500

    learning ruby sept 16

--- script.rb
+++ script.rb
@@ -1,1 +1,14 @@
-print "Hello HackerRank!!";
+# print "Hello HackerRank!!"
+
+# print self
+
+
+# def odd_or_even(number)
+#
+#   number.even?
+#
+# end
+#
+# (0...gets.to_i).each do |i|
+#   puts odd_or_even(gets.to_i)
+# end
