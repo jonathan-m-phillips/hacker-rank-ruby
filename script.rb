dad3530191b055dc0826d1621505a490d39c75dd
Jonathan Phillips <jonathan.m.phillips2@gmail.com>
2020-09-16 13:21:55 -0500

    learning iterations in ruby and watching for exceptions sept 16

--- script.rb
+++ script.rb
@@ -33,3 +33,13 @@
 #   end
 # end
 
+#----------------------------------------------
+
+def scoring(array)
+  # update_score of every user in the array unless the user is admin
+  array.each do |user|
+    unless user.is_admin?
+      user.update_score
+    end
+  end
+end
